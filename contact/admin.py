7ec13403ad0bb974a538896c91ae88a0720e7396
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2024-12-31 02:03:11 -0300

    Customizing admin.ModelAdmin options

--- contact/admin.py
+++ contact/admin.py
@@ -6,4 +6,41 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    # Itens que serão exibidos na tela de administração
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'phone',
+    )
+
+    # Ordem dos itens na tela de administração
+    ordering = ('-id',)
+
+    # Filtro dos itens na tela de administração
+    # list_filter = ('created_date',)
+
+    # Busca dos itens na tela de administração
+    search_fields = (
+        'id',
+        'first_name',
+        'last_name'
+    )
+
+    # Quantidade de itens por página na tela de administração
+    list_per_page = 20
+
+    # Quantidade máxima de itens na tela de administração
+    list_max_show_all = 200
+
+    # Editar os itens na tela de administração
+    list_editable = (
+        'first_name',
+        'last_name',
+    )
+
+    # Link de edição nos itens na tela de administração
+    list_display_links = (
+        'id',
+        'phone',
+    )
