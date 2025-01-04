577e6ca1ff87d8e36a45174fd173379b053d0d9b
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2025-01-04 14:14:49 -0300

    add Meta class for correctly plural class name in dashboard admin

--- contact/models.py
+++ contact/models.py
@@ -15,6 +15,10 @@ picture (image)
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
