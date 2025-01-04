47951bb57c106b8226281b42fd0429397a3f66eb
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2025-01-04 14:04:41 -0300

    create class category model

--- contact/models.py
+++ contact/models.py
@@ -14,6 +14,13 @@ picture (image)
 """
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -23,6 +30,12 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(upload_to='pictures/%Y/%m/', blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
