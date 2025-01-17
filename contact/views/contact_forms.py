72fb5e2457c7bbe6a2e389b984932c7be7b84437
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2025-01-17 15:02:13 -0300

    create a ContactForm(forms.ModelForm)

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,12 +1,52 @@
+from django import forms
 from django.shortcuts import render  # , get_object_or_404, redirect
 # from django.db.models import Q
 # from django.core.paginator import Paginator
-# from contact.models import Contact  # type: ignore
+from django.core.exceptions import ValidationError
+from contact.models import Contact  # type: ignore
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name',
+            'last_name',
+            'phone'
+        )
+
+    def clean(self):
+        self.add_error(
+            None,
+            ValidationError(
+                'Message of error',
+                code='invalid'
+            )
+        )
+        self.add_error(
+            None,
+            ValidationError(
+                'Message of error 2',
+                code='invalid'
+            )
+        )
+        return super().clean()
 
 
 def create(request):
-    context = {
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
 
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+
+    context = {
+        'form': ContactForm()
     }
 
     return render(
