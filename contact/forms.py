cd67910286ecee6d5d9a81b3bc2562503fb3cbdd
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2025-01-17 15:49:31 -0300

    add attrs, label and help text to first_name filed and use clean method for validation fields

--- contact/forms.py
+++ contact/forms.py
@@ -1,30 +1,73 @@
 from django import forms
-from .models import Contact  # type: ignore
 from django.core.exceptions import ValidationError
+from .models import Contact  # type: ignore
 
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'class-class',
+                'placeholder': 'John'
+            }
+        ),
+        label='Name',
+        help_text='Help text for your user'
+    )
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class': 'class-class',
+        #     'placeholder': 'John'
+        # })
+
     class Meta:
         model = Contact
+
         fields = (
             'first_name',
             'last_name',
             'phone'
         )
 
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': '',
+        #             'placeholder': 'John'
+        #         }
+        #     )
+        # }
+
     def clean(self):
-        self.add_error(
-            None,
-            ValidationError(
-                'Message of error',
+        cleaned_data = self.cleaned_data
+
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            message_error = ValidationError(
+                'The first name isn\'t equal the last name',
                 code='invalid'
             )
-        )
-        self.add_error(
-            None,
-            ValidationError(
-                'Message of error 2',
-                code='invalid'
-            )
-        )
+
+            self.add_error('first_name', message_error)
+            self.add_error('last_name', message_error)
+
         return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Message of error clean_first_name',
+                    code='invalid'
+                )
+            )
+
+        return first_name
