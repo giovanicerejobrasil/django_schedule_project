48d75543a7314a14af1fd075bbc2cbdcb2059d98
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2025-01-22 15:14:47 -0300

    create a login and logout views

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from contact.forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+from contact.forms import RegisterForm
 
 
 def register(request):
@@ -12,7 +14,7 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'User register with success!')
-            return redirect('contact:index')
+            return redirect('contact:login')
 
         return render(
             request,
@@ -25,3 +27,31 @@ def register(request):
         'contact/register.html',
         {'form': form}
     )
+
+
+def login(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            messages.success(request, 'Logged with success')
+            auth.login(request, user)
+
+            return redirect('contact:index')
+
+        messages.error(request, 'Invalid login')
+
+    return render(
+        request,
+        'contact/login.html',
+        {'form': form}
+    )
+
+
+def logout(request):
+    auth.logout(request)
+
+    return redirect('contact:login')
